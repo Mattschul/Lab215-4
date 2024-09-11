7c781af04069ac495f1dd4f30d7dc9036a5b6655
schulerdesigns <160165853+schulerdesigns@users.noreply.github.com>
2024-09-11 13:11:51 -0400

    Finalized geometric series

--- Lab215-4/Lab215-4.cpp
+++ Lab215-4/Lab215-4.cpp
@@ -1,5 +1,6 @@
 // Lab215-4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//By Matt Schuler, for flow control practice
+//CGT215 Fall 2024
 
 #include <iostream>
 using namespace std;
@@ -46,12 +47,18 @@ void factorial() {
 		
 	}
 }
+
+
 void arithmetic() {
+
 	int start(0), diff(0), num(0), sum(0);
+
 	cout << "Please enter a starting number: ";
 	cin >> start;
+
 	cout << "Please enter a number to add each time: ";
 	cin >> diff;
+
 	cout << "Please enter the number of elements in the series:";
 	cin >> num;
 
@@ -83,32 +90,74 @@ void arithmetic() {
 		}
 	}
 }
+
 void geometric() {
-	// I recommend writing your geometric series code here
+
+	int a(0), r(0), num(0), sum(0);
+
+	cout << "Please enter a starting number: ";
+	cin >> a;
+
+	cout << "Please enter a number to multiply each time: ";
+	cin >> r;
+
+	cout << "Please enter the number of elements in the series:";
+	cin >> num;
+
+	//check for negative and insult the user
+	while (num < 1) {
+		cout << "No, you are a knob. Number of elements must be positive." << endl;
+		cout << "Please enter the number of elements in the series:";
+		cin >> num;
+	}
+
+	while (num >= 0) {
+		//last term is sum
+		if (num == 0) {
+			cout << sum << endl;
+			num = num - 1;
+		}
+		//add equal sign in 2nd to last term
+		else if (num == 1) {
+			cout << a << " = ";
+			sum = sum + a;
+			num = num - 1;
+		}
+		//geometric series operation
+		else {
+			cout << a << " + ";
+			sum = sum + a;
+			a = a * r;
+			num = num - 1;
+		}
+	}
 }
-int main() {
-	int choice;
-	char again;
-	do {
-		printMenu();
-		cin >> choice;
-		// Quit if user chooses to exit (or any invalid choice)
-		if (choice > 3 || choice < 1) {
-			return 0;
-		}
-		else if (choice == 1) {
-			factorial();
-		}
-		else if (choice == 2) {
-			arithmetic();
-		}
-		else if (choice == 3) {
-			geometric();
-		}
-		cout << "Go Again? [Y/N] ";
-		cin >> again;
-	} while (again == 'y' || again == 'Y');
-}
+
+
+
+	int main() {
+		int choice;
+		char again;
+		do {
+			printMenu();
+			cin >> choice;
+			// Quit if user chooses to exit (or any invalid choice)
+			if (choice > 3 || choice < 1) {
+				return 0;
+			}
+			else if (choice == 1) {
+				factorial();
+			}
+			else if (choice == 2) {
+				arithmetic();
+			}
+			else if (choice == 3) {
+				geometric();
+			}
+			cout << "Go Again? [Y/N] ";
+			cin >> again;
+		} while (again == 'y' || again == 'Y');
+	}
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
